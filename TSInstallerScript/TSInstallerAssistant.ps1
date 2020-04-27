be76d2f72be47e75456192b5a5132a0d7299962b
sukesh kancharla <34930258+sukeshkancharla8@users.noreply.github.com>
2020-04-27 17:53:17 +0530

    Added Error Handling

--- TSInstallerScript/TSInstallerAssistant.ps1
+++ TSInstallerScript/TSInstallerAssistant.ps1
@@ -10,127 +10,143 @@ Function Log {
     $line | Out-File $LogFile -Append -Force
 }
 
-#Properties Reading
-$file_content = Get-Content "$PSScriptRoot\arguments.properties"
-$file_content = ($file_content -replace '\.', '\.') -join [Environment]::NewLine
-$properties = ConvertFrom-StringData($file_content)
+#Properties Initialization
+$properties = (ConvertFrom-StringData([Regex]::Escape((Get-Content "$PSScriptRoot/arguments.properties" -raw)) -replace "(\\r)?\\n", [Environment]::NewLine))
 $spaceRequired = $properties.spaceRequired
 $hosts = $properties.host
 $Version = $properties.TSVersion
+$destination = "$PSScriptRoot\Installers\"
+$logFile = "$PSScriptRoot\TSInstallerAssistant_log.txt"
+
+IF([string]::IsNullOrEmpty($Installer)) {
+	$Installer = $properties.installer
+}
+
+#Disk Space Checking
+Log "checking Disk space"
+$Disk = Get-WmiObject -Class Win32_logicaldisk -Filter "DeviceID = 'C:'"
+if($Disk.FreeSpace -le $spaceRequired) {
+	Log "Disk Space Insufficient"
+	return
+}
+Log "space is sufficient, ready to start operation"
 
 #Installer Path Initialization
 IF([string]::IsNullOrEmpty($Installer)) {
 	$Path = "\\etnafile02.embarcadero.com\ERStudio\Builds\ERTS\$Version\"
 	$filter="*.exe"
-	if(Test-Path $Path -PathType leaf) {
-		$latest = Get-ChildItem -Path $Path -Filter $filter | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+	if(Test-Path $Path) {
+		$latest = Get-ChildItem -Path $Path -Filter $filter | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 		$latest = $latest.name
 		If (!($Path.LastIndexOf("\")+1 -eq $Path.Length)) {
 			$Path = $Path + "\"
 		}
 		$Installer = "$Path$latest"
+	}
+	else {
+		Log "Default path is not found"
 	}
 	
 }
-
-IF((Get-Item $Installer) -is [System.IO.DirectoryInfo]) {
-	$Path = $Installer
-	$filter="*.exe"
-	if(Test-Path $Path -PathType leaf) {
-		$latest = Get-ChildItem -Path $Path -Filter $filter | Sort-Object LastAccessTime -Descending | Select-Object -First 1
-		$latest = $latest.name
-		If (!($Path.LastIndexOf("\")+1 -eq $Path.Length)) {
-			$Path = $Path + "\"
+IF($Installer) {
+	IF((Get-Item $Installer) -is [System.IO.DirectoryInfo]) {
+		$Path = $Installer
+		$filter="*.exe"
+		if(Test-Path $Path) {
+			$latest = Get-ChildItem -Path $Path -Filter $filter | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+			$latest = $latest.name
+			If (!($Path.LastIndexOf("\")+1 -eq $Path.Length)) {
+				$Path = $Path + "\"
+			}
+			$Installer = "$Path$latest"
 		}
-		$Installer = "$Path$latest"
+		else {
+			Log "given installer path is not found"
+		}
 	}
+}
+
+IF([string]::IsNullOrEmpty($Installer)) {
+	Log "Installer is not valid"
+	return
+}
+
+Log "Checking Installer Path"
+if(-Not (Test-Path $installer -PathType leaf) ) {
+	Log "Installer Not Found"
+	return
+}
+
+if(-Not (Test-Path $destination) ) {
+	Log "Creating Installers Folder"
+	New-Item -Path "$PSScriptRoot" -Name "installers" -ItemType "directory"
 }
 
 $path = Split-Path -Path $Installer
 $exeFile = Split-Path -Leaf $Installer
-$destination = "$PSScriptRoot\Installers\"
-$logFile = "$PSScriptRoot\TSInstallerAssistant_log.txt"
 
-$Disk = Get-WmiObject -Class Win32_logicaldisk -Filter "DeviceID = 'C:'"
+#Copying New Installer
+if(Test-Path $installer -PathType leaf) {
 
-Log "checking Disk space"
-if($Disk.FreeSpace -ge $spaceRequired) {
-	Log "space is sufficient, ready to start operation"
-	Log "Checking Installer Path"
-	if(-Not (Test-Path $installer -PathType leaf) ) {
-		return
-	}
-	
-    if(Test-Path $installer -PathType leaf) {
-	
-		Remove-Item $PSScriptRoot\Installers\*.exe -Force
-		Log ("copying Installer from " + $Installer)
-		Copy-Item $installer -Destination $Destination
-        
-	}
-	
-	Log "Uninstalling previous version if exists"
-	If (Get-Service EmbarcaderoTeamServer -ErrorAction SilentlyContinue) {
-		If ((Get-Service EmbarcaderoTeamServer).Status -eq 'Running') {
-			Stop-Service EmbarcaderoTeamServer
-			Log "stopping teamserver service"
-		}
-		If (Get-Service RepoSrvComm -ErrorAction SilentlyContinue) {
-			If ((Get-Service RepoSrvComm).Status -eq 'Running') {
-				Stop-Service RepoSrvComm
-				Log "stopping repo server communication service"
-			}
-		}
-		If (Get-Service RepoSrvDb -ErrorAction SilentlyContinue) {
-			If ((Get-Service RepoSrvDb).Status -eq 'Running') {
-				Stop-Service RepoSrvDb
-				Log "stopping repo server DB service"
-			}
-		}
-		
-		If (Get-Service RepoSrvEvents -ErrorAction SilentlyContinue) {
-			If ((Get-Service RepoSrvEvents).Status -eq 'Running') {
-				Stop-Service RepoSrvEvents
-				Log "stopping Repo server Events service"
-			}
-		}
-				
-		foreach($obj in Get-ChildItem "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall") {
-			$dname = $obj.GetValue("DisplayName")
-			if ($dname -match "IDERA Team Server*") {
-				Log "Uninstalling TS Version:$($obj.GetValue('BundleVersion'))"
-				$uninstExe = $obj.GetValue("BundleCachePath")
-				$p = Start-Process $uninstExe -ArgumentList "/uninstall /quiet" -Verb runas -Wait
-				If($p.ExitCode -ne 0) {
-					Log "Uninstallation Failed  with error code $($p.ExitCode)."
-					return
-				}
-				break
-			}
-		}
-	}
-	
-	Log "Checking for Backup Folder"
-	if(Test-Path $env:TEMP\TeamserverDataBackup) {
-		Log "Backup folder exists, Deleting it"
-		Remove-Item -path $env:TEMP\TeamServerDataBackup -recurse -Force
-		Log "Backup folder Deleted"
-	}
-	
-    if(Test-Path $destination$exeFile -PathType leaf) {
-		Write-Output "$destination$exeFile"
-		Log "Teamserver Installation Started"
-		$p = Start-Process $destination$exeFile -ArgumentList "/quiet /install" -Verb runas -Wait
-		If($p.ExitCode -ne 0) {
-			Log "Installation Failed with error code $($p.ExitCode)."
-			return
-		}
-		Log "Teamserver Installation Completed"
-	}
+	#Removing Exe files in Installers folder
+	Log "Cleaning Installers folder"
+	Remove-Item $PSScriptRoot\Installers\*.exe -Force
+	#Copying new Installer
+	Log ("copying Installer " + $Installer)
+	Copy-Item $installer -Destination $Destination
 	
 }
-else {
-	Log "Disk space is not sufficient"
+
+Log "Uninstalling previous version if exists"
+If (Get-Service EmbarcaderoTeamServer -ErrorAction SilentlyContinue) {
+	If ((Get-Service EmbarcaderoTeamServer).Status -eq 'Running') {
+		Stop-Service EmbarcaderoTeamServer
+		Log "stopping teamserver service"
+	}
+	If (Get-Service RepoSrvComm -ErrorAction SilentlyContinue) {
+		If ((Get-Service RepoSrvComm).Status -eq 'Running') {
+			Stop-Service RepoSrvComm
+			Log "stopping repo server communication service"
+		}
+	}
+	If (Get-Service RepoSrvDb -ErrorAction SilentlyContinue) {
+		If ((Get-Service RepoSrvDb).Status -eq 'Running') {
+			Stop-Service RepoSrvDb
+			Log "stopping repo server DB service"
+		}
+	}
+	
+	If (Get-Service RepoSrvEvents -ErrorAction SilentlyContinue) {
+		If ((Get-Service RepoSrvEvents).Status -eq 'Running') {
+			Stop-Service RepoSrvEvents
+			Log "stopping Repo server Events service"
+		}
+	}
+			
+	foreach($obj in Get-ChildItem "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall") {
+		$dname = $obj.GetValue("DisplayName")
+		if ($dname -match "IDERA Team Server*") {
+			Log "Uninstalling TS Version:$($obj.GetValue('BundleVersion'))"
+			$uninstExe = $obj.GetValue("BundleCachePath")
+			Write-Output $uninstExe
+			Start-Process $uninstExe -ArgumentList "/uninstall /quiet" -Verb runas -Wait
+			break
+		}
+	}
 }
 
+#Deleting Backup Folder if exists
+Log "Checking for Backup Folder"
+if(Test-Path $env:TEMP\TeamserverDataBackup) {
+	Log "Backup folder exists, Deleting it"
+	Remove-Item -path $env:TEMP\TeamServerDataBackup -recurse -Force
+	Log "Backup folder Deleted"
+}
+
+#Installation of new Installer
+if(Test-Path $destination$exeFile -PathType leaf) {
+	Log "Teamserver Installation Started"
+	Start-Process $destination$exeFile -ArgumentList "/quiet /install" -Verb runas -Wait
+	Log "Teamserver Installation Completed"
+}
 
